7dc7c37ee00896409b4e85acb7b829a66c8d4b0b
andrew-petrenko <akk.chat@gmail.com>
2020-06-16 17:03:25 +0300

    init

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import s "codeClinic/something"
+
+func main() {
+	println(s.GetSomething())
+	println("Hello dude")
+}
---
+++ something/main.go
@@ -0,0 +1,5 @@
+package something
+
+func GetSomething() string {
+	return "Something"
+}
